9b916456e1321d85e814f70d2cee2e7e7dd40c1f
Soumyajoy Das <70829126+soumyajoy01@users.noreply.github.com>
2021-11-12 18:36:04 +0530

    Final Website

---
+++ js/course.js
@@ -0,0 +1,120 @@
+function toggleMe(a) {
+    var e = document.getElementById(a);
+    if (!e) return true;
+    if (e.style.display == "none") {
+        e.style.display = "block"
+    } else {
+        e.style.display = "none"
+    }
+    return true;
+}
+
+const btn_1 = document.getElementById("btn1");
+
+btn_1.addEventListener("click", () => {
+
+    if (btn_1.value === "Click here for more information") {
+        btn_1.value = "Collapse";
+    } else {
+        btn_1.value = "Click here for more information";
+    }
+})
+
+const btn_2 = document.getElementById("btn2");
+
+btn_2.addEventListener("click", () => {
+
+    if (btn_2.value === "Click here for more information") {
+        btn_2.value = "Collapse";
+    } else {
+        btn_2.value = "Click here for more information";
+    }
+})
+
+const btn_3 = document.getElementById("btn3");
+
+btn_3.addEventListener("click", () => {
+
+    if (btn_3.value === "Click here for more information") {
+        btn_3.value = "Collapse";
+    } else {
+        btn_3.value = "Click here for more information";
+    }
+})
+
+const btn_4 = document.getElementById("btn4");
+
+btn_4.addEventListener("click", () => {
+
+    if (btn_4.value === "Click here for more information") {
+        btn_4.value = "Collapse";
+    } else {
+        btn_4.value = "Click here for more information";
+    }
+})
+
+const btn_5 = document.getElementById("btn5");
+
+btn_5.addEventListener("click", () => {
+
+    if (btn_5.value === "Click here for more information") {
+        btn_5.value = "Collapse";
+    } else {
+        btn_5.value = "Click here for more information";
+    }
+})
+
+const btn_6 = document.getElementById("btn6");
+
+btn_6.addEventListener("click", () => {
+
+    if (btn_6.value === "Click here for more information") {
+        btn_6.value = "Collapse";
+    } else {
+        btn_6.value = "Click here for more information";
+    }
+})
+
+const btn_7 = document.getElementById("btn7");
+
+btn_7.addEventListener("click", () => {
+
+    if (btn_7.value === "Click here for more information") {
+        btn_7.value = "Collapse";
+    } else {
+        btn_7.value = "Click here for more information";
+    }
+})
+
+const btn_8 = document.getElementById("btn8");
+
+btn_8.addEventListener("click", () => {
+
+    if (btn_8.value === "Click here for more information") {
+        btn_8.value = "Collapse";
+    } else {
+        btn_8.value = "Click here for more information";
+    }
+})
+
+const btn_9 = document.getElementById("btn9");
+
+btn_9.addEventListener("click", () => {
+
+    if (btn_9.value === "Click here for more information") {
+        btn_9.value = "Collapse";
+    } else {
+        btn_9.value = "Click here for more information";
+    }
+})
+
+const btn_10 = document.getElementById("btn10");
+
+btn_10.addEventListener("click", () => {
+
+    if (btn_10.value === "Click here for more information") {
+        btn_10.value = "Collapse";
+    } else {
+        btn_10.value = "Click here for more information";
+    }
+})
